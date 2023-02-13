1f6bdf5fb522a4fb11031c791cfcef36d853855b
KaterynaChernetska <vesnavved@gmail.com>
2023-02-13 17:30:32 +0200

    Add loading contacts from LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,24 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import Notiflix from 'notiflix';
 
+const LOCAL_STORAGE_KEY = 'arrayOfContacts';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: this.getLocalStorageContacts() ?? [],
     filter: '',
   };
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      const stringifiedContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem(LOCAL_STORAGE_KEY, stringifiedContacts);
+    }
+  }
+  getLocalStorageContacts() {
+    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  }
+
   addNewContact = newContact => {
     if (this.state.contacts.some(contact => contact.name === newContact.name)) {
       Notiflix.Notify.failure(`${newContact.name} is already in the contacts`);
@@ -27,7 +39,11 @@ export class App extends Component {
   };
 
   handleFilter = event => {
-    if(!this.state.contacts.some(contact => contact.name.includes(event.target.value))) {
+    if (
+      !this.state.contacts.some(contact =>
+        contact.name.includes(event.target.value)
+      )
+    ) {
       Notiflix.Notify.failure(`${event.target.value} is not in the contacts`);
     }
     this.setState({ filter: event.target.value });
@@ -42,10 +58,10 @@ export class App extends Component {
     });
 
     return (
-      <div style={{'textAlign': 'center'}}>
-        <h1 style={{'marginBottom': '20px'}}>Phonebook</h1>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ marginBottom: '20px' }}>Phonebook</h1>
         <ContactForm addContact={this.addNewContact} />
-        <h2 style={{'marginBottom': '20px'}}>Contacts</h2>
+        <h2 style={{ marginBottom: '20px' }}>Contacts</h2>
         <Filter value={this.state.filter} filterContacts={this.handleFilter} />
         <ContactList
           deleteContact={this.handleClick}
